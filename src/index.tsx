199b1f9d63ac75f50c875ea70ab15cd1aba8264d
Tangjj1996 <awesometangji@gmail.com>
2023-07-24 15:04:39 +0800

    feat: Vite init

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,9 @@
-export default {};
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./app";
+
+ReactDOM.createRoot(document.querySelector("#root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
