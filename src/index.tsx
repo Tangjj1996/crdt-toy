c2ff8d6e94d8ba1dd181f750b0589acb473b952a
Tangjj1996 <awesometangji@gmail.com>
2023-07-24 19:59:44 +0800

    feat: Provider replace

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ChakraBaseProvider } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import App from "./app";
 
 ReactDOM.createRoot(document.querySelector("#root")!).render(
   <React.StrictMode>
-    <ChakraBaseProvider>
+    <ChakraProvider>
       <App />
-    </ChakraBaseProvider>
+    </ChakraProvider>
   </React.StrictMode>,
 );
