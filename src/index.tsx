bda3c11347cee4b36fffd5dc365dd9258ad08b54
Tangjj1996 <awesometangji@gmail.com>
2023-07-24 15:32:07 +0800

    feat: Add chakra-ui

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ChakraBaseProvider } from "@chakra-ui/react";
 import App from "./app";
 
 ReactDOM.createRoot(document.querySelector("#root")!).render(
   <React.StrictMode>
-    <App />
+    <ChakraBaseProvider>
+      <App />
+    </ChakraBaseProvider>
   </React.StrictMode>,
 );
